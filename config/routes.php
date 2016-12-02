6be58e74ce4d3aa9a71c81f3cdc2976294ca0677
venom3333 <venom3333@gmail.com>
2016-12-02 19:49:27 +0300

    Улучшил ЧПУ (короткие), добавил базовый функционал всей навигации

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,7 @@
 /**
  * файл конфигурации маршрутов
  */
-//$this->addRoute( 'category', [ 'controller' => 'category', 'action' => 'index' ] );
-//$this->addRoute( 'index', [ 'controller' => 'index', 'action' => 'index' ] );
-//$this->addRoute( '', [ 'controller' => 'index', 'action' => 'index' ] );
 
 // Маршруты по умолчанию
 $this->addRoute( '^$', [ 'controller' => 'Index', 'action' => 'index' ] );
-$this->addRoute( "^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)?/(?P<params>[0-9-]+)?$" );
-$this->addRoute( "^(?P<controller>[a-z-]+)/(?P<params>[0-9-]+)?$", ['action' => 'index'] );
+$this->addRoute( "^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?/?(?P<params>[0-9-]+)?$" );
