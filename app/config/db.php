b739c415a4f667a9bf65f1c3ee6f56f1b8cceef8
venom3333 <venom3333@gmail.com>
2016-12-21 10:46:15 +0300

    Добавлена временная страница с паролем для босса (подготовка к переносу на боевую машину)

--- app/config/db.php
+++ app/config/db.php
@@ -3,7 +3,14 @@
  * настройки БД
  */
 
-define('DB_USER', 'root');
-define('DB_NAME', 'custom_light');
-define('DB_HOST', 'localhost');
-define('DB_PASS', '666666');
+if ( stristr( $_SERVER ['DOCUMENT_ROOT'], 'localhost' ) ) {
+	define( 'DB_USER', 'venom' );
+	define( 'DB_NAME', 'custom_light' );
+	define( 'DB_HOST', 'localhost' );
+	define( 'DB_PASS', '666666' );
+} else {
+	define( 'DB_USER', 'customligh_venom' );
+	define( 'DB_NAME', 'customligh_db' );
+	define( 'DB_HOST', 'localhost' );
+	define( 'DB_PASS', '666666' );
+}
--- www/index.php
+++ www/index.php
@@ -1,15 +1,26 @@
 <?php
 // 1. Общие настройки
 session_start();
+
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
 
 // 2. Константы
 define( 'WWW', __DIR__ );
 define( 'ROOT', dirname( __DIR__ ) );
-define( 'APP', dirname( __DIR__ ) . '/app'  );
+define( 'APP', dirname( __DIR__ ) . '/app' );
 define( 'CONFIG', dirname( __DIR__ ) . '/app/config/config.php' );
 define( 'CORE', dirname( __DIR__ ) . '/core/core.php' );
+
+// Если не босс, то временная главная страница
+if ( isset ($_POST['boss']) && $_POST['boss'] == 'IamTheBOSS'){
+	$_SESSION ['boss'] = $_POST['boss'];
+}
+if ( ! isset( $_SESSION ['boss'] ) || $_SESSION ['boss'] != 'IamTheBOSS' ) {
+	header( 'Location: placeholder/index.html' );
+	exit();
+}
+// /Если не босс, то временная главная страница
 
 // 2. Подключение файлов
 require_once CONFIG; // Инициализация настроек
@@ -19,11 +30,11 @@ require_once CORE;   // Подключение компонентов фрейм
 // функция автозагрузки файлов классов
 spl_autoload_register( function ( $class ) {
 
-	if ( file_exists( $file = ROOT . '/' . str_replace( '\\', '/', $class) . '.php' ) ) {
+	if ( file_exists( $file = ROOT . '/' . str_replace( '\\', '/', $class ) . '.php' ) ) {
 		require_once $file;
-	}
-	else {
-		echo "Не найден файл Класса: $class.php"; die;
+	} else {
+		echo "Не найден файл Класса: $class.php";
+		die;
 	}
 } );
 
