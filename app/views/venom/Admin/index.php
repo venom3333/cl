f814d8f1f427ab9bf82e6d92793ad7a2adbe3841
venom3333 <venom3333@gmail.com>
2016-12-25 23:34:36 +0300

    Добавлены некоторые константы для размеров изображений

--- app/views/venom/Admin/index.php
+++ app/views/venom/Admin/index.php
@@ -1,5 +1,5 @@
 <h1>Админка. Главная.</h1>
 <h3>Работа с базой данных:</h3>
-<a href="/admin/products"><input type="button" class="btn-danger" value="Продукты"></a>
-<a href="/admin/projects"><input type="button" class="btn-danger" value="Проекты"></a>
+<a href="/admin/products"><input type="button" class="btn-success" value="Продукты"></a>
+<a href="/admin/projects"><input type="button" class="btn-warning" value="Проекты"></a>
 <a href="/admin/categories"><input type="button" class="btn-danger" value="Категории"></a>
