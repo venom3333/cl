c4939a3e7dcc1d9b627e3b07045238736040fe79
venom3333 <venom3333@gmail.com>
2017-01-03 15:55:37 +0300

    Реализовано добавление и редактирование информационных страниц (а так же автоматизация навигационного меню для этого)

--- app/views/venom/Admin/index.php
+++ app/views/venom/Admin/index.php
@@ -3,3 +3,4 @@
 <a href="/admin/products"><input type="button" class="btn-success" value="Продукты"></a>
 <a href="/admin/projects"><input type="button" class="btn-warning" value="Проекты"></a>
 <a href="/admin/categories"><input type="button" class="btn-danger" value="Категории"></a>
+<a href="/admin/pages"><input type="button" class="btn-default" value="Страницы"></a>
