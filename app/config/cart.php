74d8d99a238b2f082c24ae8af4764c3aa1e66364
Venom <venom3333@gmail.com>
2017-01-08 18:35:40 +0300

    Временный костыль для верной обработки функцией htmlSpecialCharsUniversal() переданного объекта JSON методом POST

--- app/config/cart.php
+++ app/config/cart.php
@@ -4,24 +4,28 @@
  * Файл инициализации корзины
  *
  */
-if ( ! isset( $_SESSION['cart']['grandQuantity'] ) || $_SESSION['cart']['grandQuantity'] < 0 ) {
-	$_SESSION['cart']['grandQuantity'] = 0;
+if (!isset($_SESSION['cart']['grandQuantity']) || $_SESSION['cart']['grandQuantity'] < 0) {
+    $_SESSION['cart']['grandQuantity'] = 0;
 } else {
-	$_SESSION['cart']['grandQuantity'] = 0;
-	if ( isset( $_SESSION['cart']['products'] ) ) {
-		foreach ( $_SESSION['cart']['products'] as $product ) {
-			$_SESSION['cart']['grandQuantity'] += $product->quantity;
-		}
-	}
+    $_SESSION['cart']['grandQuantity'] = 0;
+    if (isset($_SESSION['cart']['products'])) {
+        foreach ($_SESSION['cart']['products'] as $product) {
+            if ($product) {
+                $_SESSION['cart']['grandQuantity'] += $product->quantity;
+            }
+        }
+    }
 }
 
-if ( ! isset( $_SESSION['cart']['grandTotal'] ) || $_SESSION['cart']['grandTotal'] < 0 ) {
-	$_SESSION['cart']['grandTotal'] = 0;
+if (!isset($_SESSION['cart']['grandTotal']) || $_SESSION['cart']['grandTotal'] < 0) {
+    $_SESSION['cart']['grandTotal'] = 0;
 } else {
-	$_SESSION['cart']['grandTotal'] = 0;
-	if ( isset( $_SESSION['cart']['products'] ) ) {
-		foreach ( $_SESSION['cart']['products'] as $product ) {
-			$_SESSION['cart']['grandTotal'] += $product->quantity * $product->price;
-		}
-	}
+    $_SESSION['cart']['grandTotal'] = 0;
+    if (isset($_SESSION['cart']['products'])) {
+        foreach ($_SESSION['cart']['products'] as $product) {
+            if ($product) {
+                $_SESSION['cart']['grandTotal'] += $product->quantity * $product->price;
+            }
+        }
+    }
 }
--- core/base/Controller.php
+++ core/base/Controller.php
@@ -2,162 +2,174 @@
 
 namespace core\base;
 
-abstract class Controller {
-	/**
-	 * Текущий маршрут и параметры (controller, action, params, alias)
-	 * @var array
-	 */
-	public $route = array();
-	/**
-	 * Текущий вид
-	 * @var string
-	 */
-	public $view;
-	/**
-	 * Текущий шаблон
-	 * @var string
-	 */
-	public $layout = 'default';
-	/**
-	 * Пользовательские данные
-	 * @var array
-	 */
-	public $vars = [];
+abstract class Controller
+{
+    /**
+     * Текущий маршрут и параметры (controller, action, params, alias)
+     * @var array
+     */
+    public $route = array();
+    /**
+     * Текущий вид
+     * @var string
+     */
+    public $view;
+    /**
+     * Текущий шаблон
+     * @var string
+     */
+    public $layout = 'default';
+    /**
+     * Пользовательские данные
+     * @var array
+     */
+    public $vars = [];
 
-	/**
-	 * общая информация для определенного шаблона (меню и т.п.)
-	 * @var array
-	 */
-	protected $layoutEssentials = array();
+    /**
+     * общая информация для определенного шаблона (меню и т.п.)
+     * @var array
+     */
+    protected $layoutEssentials = array();
 
-	/**
-	 * Нужна ли авторизация
-	 * @var bool;
-	 */
-	protected $auth = false;
+    /**
+     * Нужна ли авторизация
+     * @var bool;
+     */
+    protected $auth = false;
 
-	/**
-	 * Наименование категории авторизации (для разграничения уровней доступа)
-	 * @var bool;
-	 */
-	protected $authCategory = 'auth';
+    /**
+     * Наименование категории авторизации (для разграничения уровней доступа)
+     * @var bool;
+     */
+    protected $authCategory = 'auth';
 
-	/**
-	 * Пароль доступа к контроллеру по умолчанию
-	 * @var string;
-	 */
-	protected $pass = ADMIN_PASS;
+    /**
+     * Пароль доступа к контроллеру по умолчанию
+     * @var string;
+     */
+    protected $pass = ADMIN_PASS;
 
-	/**
-	 * Получен ли доступ к контроллеру
-	 * @var bool;
-	 */
-	protected $is_auth = false;
+    /**
+     * Получен ли доступ к контроллеру
+     * @var bool;
+     */
+    protected $is_auth = false;
 
-	/**
-	 * Controller constructor.
-	 * Создает объект контроллера, проверяет авторизацию и присваивает $route и $view
-	 *
-	 * @param array $route массив с маршрутом
-	 */
-	public function __construct( $route ) {
-		//> защита от чужих скриптов
-		if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-			$_POST = $this->htmlSpecialCharsUniversal( $_POST );
-		}
-		//< защита от чужих скриптов
-		$this->route = $route;
-		$this->view  = $route['action'];
-		//> блок функционала авторизации
-		if ( $this->auth ) {
-			$this->is_auth() or die( "АВТОРИЗАЦИЯ НЕ ПРОШЛА!!!" );
-		}
-		//< блок функционала авторизации
-		$this->getLayoutEssentials();
-	}
+    /**
+     * Controller constructor.
+     * Создает объект контроллера, проверяет авторизацию и присваивает $route и $view
+     *
+     * @param array $route массив с маршрутом
+     */
+    public function __construct($route)
+    {
+        //> защита от чужих скриптов
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = $this->htmlSpecialCharsUniversal($_POST);
+        }
 
-	/**
-	 * Проверяет авторизован ли пользователь
-	 *
-	 * @return bool авторизован или нет
-	 */
-	protected function is_auth() {
-		if ( isset ( $_POST[ $this->authCategory ] ) && password_verify( htmlspecialchars( $_POST [ $this->authCategory ] ), $this->pass ) ) {
-			$_SESSION [ $this->authCategory ] = true;
-		}
+        //< защита от чужих скриптов
+        $this->route = $route;
+        $this->view = $route['action'];
+        //> блок функционала авторизации
+        if ($this->auth) {
+            $this->is_auth() or die("АВТОРИЗАЦИЯ НЕ ПРОШЛА!!!");
+        }
+        //< блок функционала авторизации
+        $this->getLayoutEssentials();
+    }
 
-		if ( $this->view == 'index' ) { // т.к. индексные страницы, как правило, являются страницами авторизации
-			return true;
-		}
+    /**
+     * Проверяет авторизован ли пользователь
+     *
+     * @return bool авторизован или нет
+     */
+    protected function is_auth()
+    {
+        if (isset ($_POST[$this->authCategory]) && password_verify(htmlspecialchars($_POST [$this->authCategory]), $this->pass)) {
+            $_SESSION [$this->authCategory] = true;
+        }
 
-		if ( isset( $_SESSION [ $this->authCategory ] ) && $_SESSION [ $this->authCategory ] ) {
-			$this->is_auth = true;
+        if ($this->view == 'index') { // т.к. индексные страницы, как правило, являются страницами авторизации
+            return true;
+        }
 
-			return true;
-		} else {
-			$this->is_auth                    = false;
-			$_SESSION [ $this->authCategory ] = false;
+        if (isset($_SESSION [$this->authCategory]) && $_SESSION [$this->authCategory]) {
+            $this->is_auth = true;
 
-			return false;
-		}
-	}
+            return true;
+        } else {
+            $this->is_auth = false;
+            $_SESSION [$this->authCategory] = false;
 
-	/**
-	 * Отменяет авторизацию определенного контроллера
-	 */
-	public function exitAuthAction() {
-		unset( $_SESSION [ $this->authCategory ] );
-		$this->is_auth = false;
-		$this->view    = "index";
-		$title         = "Авторизация прекращена.";
-		$this->setVars( compact( 'title' ) );
-	}
+            return false;
+        }
+    }
 
-	/**
-	 * Обрабатывает строки или массивы строк функцией htmlspecialchars();
-	 *
-	 * @param array|string data
-	 *
-	 * @return array|string
-	 */
-	protected function htmlSpecialCharsUniversal( $data ) {
-		foreach ( $data as &$value ) {
-			if ( is_string( $value ) ) {
-				$value = htmlspecialchars( $value );
-			}
-		}
+    /**
+     * Отменяет авторизацию определенного контроллера
+     */
+    public function exitAuthAction()
+    {
+        unset($_SESSION [$this->authCategory]);
+        $this->is_auth = false;
+        $this->view = "index";
+        $title = "Авторизация прекращена.";
+        $this->setVars(compact('title'));
+    }
 
-		return $data;
-	}
+    /**
+     * Обрабатывает строки или массивы строк функцией htmlspecialchars();
+     *
+     * @param array|string data
+     *
+     * @return array|string
+     */
+    protected function htmlSpecialCharsUniversal($data)
+    {
+        //Костыль c JSON объектом вылечить (['data'])
+        foreach ($data as $key => &$value) {
+            if ($key != 'data') {
+                if (is_string($value)) {
+                    $value = htmlspecialchars($value);
+                }
+            }
+        }
 
-	/**
-	 * Загружает необходимую информацию для определенного шаблона
-	 */
-	protected abstract function getLayoutEssentials();
+        return $data;
+    }
 
-	/**
-	 *  Получить вид, передать в него параметры и отрисовать
-	 */
-	public function getView() {
-		$viewObject = new View( $this->route, $this->layout, $this->view );
-		$viewObject->render( $this->vars );
-	}
+    /**
+     * Загружает необходимую информацию для определенного шаблона
+     */
+    protected abstract function getLayoutEssentials();
 
-	/**
-	 *  Присвоить пользовательские данные
-	 *
-	 * @param array $vars
-	 */
-	protected function setVars( $vars ) {
-		$this->vars = $vars;
-	}
+    /**
+     *  Получить вид, передать в него параметры и отрисовать
+     */
+    public function getView()
+    {
+        $viewObject = new View($this->route, $this->layout, $this->view);
+        $viewObject->render($this->vars);
+    }
 
-	/**
-	 * Присвоить пароль для доступа к контроллеру
-	 *
-	 * @param string $pass
-	 */
-	protected function setPass( string $pass ) {
-		$this->pass = $pass;
-	}
+    /**
+     *  Присвоить пользовательские данные
+     *
+     * @param array $vars
+     */
+    protected function setVars($vars)
+    {
+        $this->vars = $vars;
+    }
+
+    /**
+     * Присвоить пароль для доступа к контроллеру
+     *
+     * @param string $pass
+     */
+    protected function setPass(string $pass)
+    {
+        $this->pass = $pass;
+    }
 }
